c6b8efcb5f368ac4dbdc424a09ec4aa8d3f440a7
Ali <shabazali.ali@siemens.com>
2020-09-18 16:14:51 +0530

    Added local storage implementation for the data to persist when even when we refresh the application

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,11 +1,14 @@
-import React, { createContext, useReducer } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { bookReducer } from '../reducers/BookReducer';
 
 
 export const BookContent = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer ,[]);
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData): [];
+    });
 
     // const addBook = (title, author) => {
     //     const newBooks = [...books, {title, author, id: uuidv1()}];
@@ -17,6 +20,10 @@ const BookContextProvider = (props) => {
     //     const newBooks = books.filter(book => book.id !== id);
     //     setBooks(newBooks);
     // }
+
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books])
 
     return ( 
         <BookContent.Provider value={{books, dispatch}}>
